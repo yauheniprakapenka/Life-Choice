61f08cbec1605ddaa8f7b4115cb11f966b9375fa
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-07-20 12:53:04 +0300

    Created new project

---
+++ LifeChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeChoice
+//
+//  Created by yauheni prakapenka on 20/07/2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
