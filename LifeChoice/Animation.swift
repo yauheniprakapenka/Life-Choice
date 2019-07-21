1568168611ad26d3fce60381a5a7b816e4783691
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-07-21 07:27:15 +0300

    Added introdution

---
+++ LifeChoice/Animation.swift
@@ -0,0 +1,40 @@
+//
+//  Animation.swift
+//  LifeChoice
+//
+//  Created by yauheni prakapenka on 20/07/2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Animation {
+    func showImageViewWithDelay(toView: UIImageView, delay: Double, alpha: CGFloat) {
+        let imageView = toView
+        UIView.animate(withDuration: 1, delay: delay, options: [], animations: {
+            imageView.alpha = alpha
+        }, completion: nil)
+    }
+    
+    func showSwitchWithDelay(toView: UISwitch, delay: Double, alpha: CGFloat) {
+        let uiSwitch = toView
+        UIView.animate(withDuration: 1, delay: delay, options: [], animations: {
+            uiSwitch.alpha = alpha
+        }, completion: nil)
+    }
+    
+    func showLabelWithDelay(toView: UILabel, delay: Double, alpha: CGFloat, text: String) {
+        let uiLabel = toView
+        toView.text = text
+        UIView.animate(withDuration: 1, delay: delay, options: [], animations: {
+            uiLabel.alpha = alpha
+        }, completion: nil)
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { timer in
+            UIImageView.animate(withDuration: 1) { [weak self] in
+            toView.alpha = 0
+            }
+        })
+    }
+}
--- LifeChoice/ViewController.swift
+++ LifeChoice/ViewController.swift
@@ -9,52 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var previousChoice = Choice.sleep
+    var currentChoice = Choice.sleep
+    
+    let motionEffect = MotionEffect()
+    let animation = Animation()
+    
+    @IBOutlet weak var backgroundColorImageView: UIImageView!
+    
+    @IBOutlet weak var socialLifeColorImageView: UIImageView!
+    @IBOutlet weak var sleepColorImageView: UIImageView!
+    @IBOutlet weak var workColorImageView: UIImageView!
+    
+    @IBOutlet weak var socialLifeDesaturateImageView: UIImageView!
+    @IBOutlet weak var workDesaturateImageView: UIImageView!
+    @IBOutlet weak var sleepDesaturateImageView: UIImageView!
+    
+    @IBOutlet weak var socialLifeSwitch: UISwitch!
+    @IBOutlet weak var workSwitch: UISwitch!
+    @IBOutlet weak var sleepSwitch: UISwitch!
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     enum Choice {
         case work
         case socialLife
         case sleep
     }
     
-    var previousChoice = Choice.sleep
-    var currentChoice = Choice.sleep
-    
-    let motionEffect = MotionEffect()
-    
-    @IBOutlet weak var socialLifeImageView: UIImageView!
-    @IBOutlet weak var sleepImageView: UIImageView!
-    @IBOutlet weak var workImageView: UIImageView!
-    @IBOutlet weak var backgroundImageView: UIImageView!
-    
-    @IBOutlet weak var workSwitch: UISwitch!
-    @IBOutlet weak var socialLifeSwitch: UISwitch!
-    @IBOutlet weak var sleepSwitch: UISwitch!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        workImageView.alpha = 1
-        socialLifeImageView.alpha = 0
-        sleepImageView.alpha = 0
+        backgroundColorImageView.alpha = 0
         
-        motionEffect.applyMotionEffect(toView: backgroundImageView, magnitude: 40)
+        socialLifeColorImageView.alpha = 0
+        sleepColorImageView.alpha = 0
+        workColorImageView.alpha = 0
+        
+        socialLifeDesaturateImageView.alpha = 0
+        workDesaturateImageView.alpha = 0
+        sleepDesaturateImageView.alpha = 0
+        
+        socialLifeSwitch.alpha = 0
+        workSwitch.alpha = 0
+        sleepSwitch.alpha = 0
+        
+        label1.alpha = 0
+
+        motionEffect.applyMotionEffect(toView: backgroundColorImageView, magnitude: 40)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animation.showImageViewWithDelay(toView: backgroundColorImageView, delay: 7, alpha: 1)
+        animation.showImageViewWithDelay(toView: socialLifeColorImageView, delay: 7, alpha: 0)
+        animation.showImageViewWithDelay(toView: sleepColorImageView, delay: 7, alpha: 0)
+        animation.showImageViewWithDelay(toView: workColorImageView, delay: 7, alpha: 0)
+        animation.showImageViewWithDelay(toView: socialLifeDesaturateImageView, delay: 1, alpha: 1)
+        animation.showImageViewWithDelay(toView: workDesaturateImageView, delay: 7, alpha: 1)
+        animation.showImageViewWithDelay(toView: sleepDesaturateImageView, delay: 7, alpha: 1)
+        
+        animation.showSwitchWithDelay(toView: socialLifeSwitch, delay: 7, alpha: 1)
+        animation.showSwitchWithDelay(toView: workSwitch, delay: 7, alpha: 1)
+        animation.showSwitchWithDelay(toView: sleepSwitch, delay: 7, alpha: 1)
+        
+        animation.showLabelWithDelay(toView: label1, delay: 3, alpha: 1, text:  "created by yauheni prakapenka")
     }
     
     @IBAction func toggleWorkSwitch(_ sender: UISwitch) {
         if !workSwitch.isOn {
-            workImageView.alpha = 0
+            workColorImageView.alpha = 0
         }
         
         currentChoice = .work
         if workSwitch.isOn && socialLifeSwitch.isOn && sleepSwitch.isOn {
             socialLifeSwitch.isOn = false
-            socialLifeImageView.alpha = 0
+            socialLifeColorImageView.alpha = 0
         }
-        workImageView.alpha = 1
+        workColorImageView.alpha = 1
         previousChoice = currentChoice
         if workSwitch.isOn {
-            workImageView.alpha = 1
+            workColorImageView.alpha = 1
         } else {
-            workImageView.alpha = 0
+            workColorImageView.alpha = 0
         }
     }
     
@@ -63,18 +101,18 @@ class ViewController: UIViewController {
         if workSwitch.isOn && socialLifeSwitch.isOn && sleepSwitch.isOn {
             if previousChoice == .work {
                 sleepSwitch.isOn = false
-                sleepImageView.alpha = 0
+                sleepColorImageView.alpha = 0
             } else {
                 workSwitch.isOn = false
-                workImageView.alpha = 0
+                workColorImageView.alpha = 0
             }
-            socialLifeImageView.alpha = 1
+            socialLifeColorImageView.alpha = 1
         }
         previousChoice = currentChoice
         if socialLifeSwitch.isOn {
-            socialLifeImageView.alpha = 1
+            socialLifeColorImageView.alpha = 1
         } else {
-            socialLifeImageView.alpha = 0
+            socialLifeColorImageView.alpha = 0
         }
     }
     
@@ -83,17 +121,17 @@ class ViewController: UIViewController {
         if workSwitch.isOn && socialLifeSwitch.isOn && sleepSwitch.isOn {
             if previousChoice == .socialLife {
                 workSwitch.isOn = false
-                socialLifeImageView.alpha = 1
-                workImageView.alpha = 0
+                socialLifeColorImageView.alpha = 1
+                workColorImageView.alpha = 0
             } else {
                 socialLifeSwitch.isOn = false
-                socialLifeImageView.alpha = 0
+                socialLifeColorImageView.alpha = 0
             }
         }
         if sleepSwitch.isOn {
-            sleepImageView.alpha = 1
+            sleepColorImageView.alpha = 1
         } else {
-            sleepImageView.alpha = 0
+            sleepColorImageView.alpha = 0
         }
         previousChoice = currentChoice
     }
